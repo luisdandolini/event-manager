7d561a77ef37d7f8e41e4f622c076c001a493649
Luis Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2026-01-14 17:41:25 -0300

    feat: configura tanStack

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -7,15 +9,15 @@ async function enableMocking() {
   if (import.meta.env.MODE !== "development") return;
 
   const { worker } = await import("./mocks/browser");
-  await worker.start({
-    onUnhandledRequest: "warn",
-  });
+  await worker.start({ onUnhandledRequest: "warn" });
 }
 
 enableMocking().then(() => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </StrictMode>
   );
 });
